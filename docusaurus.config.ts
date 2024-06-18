740219e016ce06cbcab2a37942ed8bbe594b7541
tangyu <1657904046@qq.com>
2024-06-18 16:56:49 +0800

    feat: 项目初始化

---
+++ docusaurus.config.ts
@@ -0,0 +1,169 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "SITE",
+  tagline: "保持学习的态度是非常酷的",
+  favicon: "img/favicon.ico",
+  themes: ["@docusaurus/theme-search-algolia"],
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "zh",
+    locales: ["zh", "en"],
+    localeConfigs: {
+      // 语言配置
+      en: {
+        htmlLang: "en",
+      },
+      zh: {
+        htmlLang: "zh",
+        // direction: "ltr",
+      },
+    },
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+  themeConfig: {
+    // Replace with your project's social card
+    algolia: {
+      // The application ID provided by Algolia
+      appId: "YOUR_APP_ID",
+
+      // Public API key: it is safe to commit it
+      apiKey: "YOUR_SEARCH_API_KEY",
+
+      indexName: "YOUR_INDEX_NAME",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: "external\\.com|domain\\.com",
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: "/docs/", // or as RegExp: /\/docs\//
+        to: "/",
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+
+      //... other Algolia params
+    },
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "KeepUp",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "文档",
+        },
+        { to: "/blog", label: "日志", position: "left" },
+        { type: "localeDropdown", position: "right" },
+        {
+          href: "https://github.com/facebook/docusaurus",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "文档",
+          items: [
+            {
+              label: "Intro",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "社区",
+          items: [
+            {
+              label: "堆栈溢出",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "不一致性",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "更多",
+          items: [
+            {
+              label: "日志",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ i18n/zh/docusaurus-plugin-content-docs/current/intro.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+edit_this_button: false
+---
+
+这里是中文文档介绍
